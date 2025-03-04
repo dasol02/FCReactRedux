076114ae9113649c150ed4603122491f103fbbc6
dslee02 <dslee@aladin.co.kr>
2025-03-04 16:33:27 +0900

    part 2-7 class 컴포넌트

--- reactproject01/src/app.js
+++ reactproject01/src/app.js
@@ -1,31 +1,20 @@
 /* @jsx createElement */
 
-import { render, createElement } from "./react.js";
+import { render, createElement, Component } from "./react.js";
 
-// const vdom = createElement(
-//   "P",
-//   {},
-//   createElement("h1", {}, "React 만들기"),
-//   createElement(
-//     "ul",
-//     {},
-//     createElement("li", { style: "color:red" }, "첫 번째 아이템"),
-//     createElement("li", { style: "color:blue" }, "두 번째 아이템"),
-//     createElement("li", { style: "color:green" }, "세 번째 아이템")
-//   )
-// );
-
-function Title(props) {
-  return <h1>{props.children}</h1>;
+class Title extends Component {
+  render() {
+    return <h1>{this.props.children}</h1>;
+  }
 }
 
 function Item(props) {
   return <li style={`color:${props.color}`}>{props.children}</li>;
 }
 
-const vdom = (
+const App = () => (
   <p>
-    <Title label="React">React 정말 잘 만들기</Title>
+    <Title label="React">React 정말 클래스 컴포넌트 잘 만들기</Title>
     <ul>
       <Item color="red">첫 번째 아이템</Item>
       <Item color="blue">두 번째 아이템</Item>
@@ -34,5 +23,4 @@ const vdom = (
   </p>
 );
 
-console.log(vdom);
-render(vdom, document.querySelector("#root"));
+render(<App />, document.querySelector("#root"));
